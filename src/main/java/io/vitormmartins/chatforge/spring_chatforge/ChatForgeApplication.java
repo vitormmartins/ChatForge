c6211a1ecd016d412b8f086638fdebf6a6f968c4
Vitor Matosinho Martins <vitor_m_martins@yahoo.com.br>
2025-02-17 00:37:00 +0000

    first commit

---
+++ src/main/java/io/vitormmartins/chatforge/spring_chatforge/ChatForgeApplication.java
@@ -0,0 +1,13 @@
+package io.vitormmartins.chatforge.spring_chatforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatForgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/vitormmartins/chatforge/spring_chatforge/ChatForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.vitormmartins.chatforge.spring_chatforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
